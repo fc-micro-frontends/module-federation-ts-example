94b5cf275a71672faf9ad4fdb0536b1f22415cb2
Woongjae Lee <2woongjae@gmail.com>
2023-11-26 22:40:32 +0900

    init: 다른 서버의 React 컴포넌트에 TypeScript 의 타입 설정하기

---
+++ apps/main-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
+
+import "./index.css";
+
+const Button = React.lazy(() => import("component_app/Button"));
+
+const App = () => (
+  <div className="container">
+    <div>Name: main-app</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Button
+          onClick={() => {
+            console.log("Clicked!!");
+          }}
+        >
+          Primary
+        </Button>
+      </React.Suspense>
+    </ErrorBoundary>
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Button type="warning">Warning</Button>
+      </React.Suspense>
+    </ErrorBoundary>
+  </div>
+);
+ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
